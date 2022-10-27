352ff9bc696046ddc313e962306bc495ba0ee745
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2022-10-27 21:47:56 +0200

    Set parameter.

--- plotStats.R
+++ plotStats.R
@@ -1,9 +1,10 @@
 library(tidyverse)
-readr::read_tsv("tmp.out", col_names=c("ctg", "start", "end", "mol_cov", "middle_mol_cov", "mol_len", "read_dens", "# start", "# end")) |>
+args <- commandArgs(TRUE)
+readr::read_tsv(args[1], col_names=c("ctg", "start", "end", "mol_cov", "middle_mol_cov", "mol_len", "read_dens", "# start", "# end")) |>
   dplyr::select(-c("ctg", "start", "end")) |>
   tidyr::pivot_longer(cols = everything(), names_to = "data", values_to = "values") |>
   dplyr::mutate(data = factor(data)) |>
   ggplot(aes(x = values)) +
     geom_histogram() +
-    facet_wrap(vars(data), scales = "free") + xlim(0, 0.1) #  + xlim(0, 250)
+    facet_wrap(vars(data), scales = "free") # + xlim(0, 0.1) #  + xlim(0, 250)
 
