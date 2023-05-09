767be5352c1d9c68c04ed10ffa53a8ce3350e6dc
Matthias Zytnicki <matthias.zytnicki@inra.fr>
2023-05-09 15:59:18 +0200

    Remove useless files

--- create_bed_file.h
+++
@@ -1,4 +0,0 @@
-#include <string>
-#include <vector>
-
-void createBed (std::vector<std::string> tmpfiles, std::string contigSizeFile, std::string bedFile);
--- stat_per_interval.h
+++
@@ -1,4 +0,0 @@
-#include <string>
-
-
-void statistics_per_interval (std::string stat_file, int window, std::string stat_per_interval_file);
